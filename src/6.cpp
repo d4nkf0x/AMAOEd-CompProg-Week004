1770281ab5920c18d11ea9f95b1d3e6cd14dd836
d4nkf0x <jjustoe04@gmail.com>
2021-02-18 22:10:28 +0700

    New Programs

---
+++ src/6.cpp
@@ -0,0 +1,49 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+    char name[100],password[100],address[100];
+    cout << "Enter user's name: ";
+    cin.getline(name,sizeof(name));
+    cout << "Enter user's address: ";
+    cin.getline(address,sizeof(address));
+    cout << "Enter user's password: ";
+    cin.getline(password,sizeof(password));
+    cout << "Hi, I am "<<name<<". I live at "<<address<<". My password is "<<password<<"." << endl;
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    _pause();
+    return EXIT_SUCCESS;
+}
+// 
